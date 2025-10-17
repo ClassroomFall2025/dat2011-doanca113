1536598aef9f2087044b8a8c92f8ea157758ebad
doanca113 <doanca2006@gmail.com>
2025-10-17 14:34:11 +0000

    e nộp bài

--- sinhvien.py
+++ sinhvien.py
@@ -11,6 +11,29 @@ class sinhvien:
     def xuat_thong_tin(self):
         print(f"sinh viên:{self.Ten_Sinh_Vien}, sinh năm{self.nam_sinh}có điểm môn{self.diem}")
 
-sv1=sinhvien()
-sv1.them_sinh_vien("Đoàn Cả", 2006, 9)
+class sinhvienxldl():
+  
+    #các phương thức
+    def __init__(self, ten_sv, nam_sinh, diem_mon, lap_trinh):
+        self.Ten_Sinh_Vien = ten_sv
+        self.nam_sinh = nam_sinh
+        self.diem = diem_mon
+        self.lap_trinh= lap_trinh
+
+    def xuat_thong_tin(self):
+        print(f"sinh viên:{self.Ten_Sinh_Vien}, sinh năm{self.nam_sinh}có điểm môn{self.diem},và học lập trình:{self.lap_trinh}")
+
+sv1=sinhvienxldl("Đoàn Cả",2006,9,"Python")
+sv1.xuat_thong_tin()
+
+class sinhvienxldl(sinhvien):
+    def __init__(self, ten_sv, nam_sinh, diem_mon, lap_trinh):
+        sinhvien.__init__(self, ten_sv, nam_sinh, diem_mon)
+        self.lap_trinh= lap_trinh
+
+    def xuat_thong_tin(self):
+        print(f"{sinhvien.__str__(self)} và học lập trình:{self.lap_trinh}")
+    def __str__(self):
+        return f"sinh viên:{self.Ten_Sinh_Vien}, sinh năm{self.nam_sinh}có điểm môn{self.diem}"
+sv1=sinhvienxldl("Đoàn Cả",2006,9,"Python")
 sv1.xuat_thong_tin()
